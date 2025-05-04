0a9d57e071f64b20e78039476069bf8edf665dae
masterchen-dev <2374517599@qq.com>
2025-05-04 22:48:24 +0800

    初始化提交

---
+++ testgit/src/com/pojo/Student.java
@@ -0,0 +1,11 @@
+package com.pojo;
+
+public class Student {
+    public String name;
+    public String password;
+
+    public Student(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
---
+++ testgit/src/com/test/Test1.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import com.pojo.Student;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Student student=new Student("chen","111");
+        System.out.println(student.name);
+
+    }
+}
