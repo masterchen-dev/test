fe992b7939d15454cf27e65b7409006110d16989
masterchen-dev <2374517599@qq.com>
2025-05-05 17:11:42 +0800

    Student类新增age属性

--- testgit/src/com/pojo/Student.java
+++ testgit/src/com/pojo/Student.java
@@ -3,9 +3,11 @@ package com.pojo;
 public class Student {
     public String name;
     public String password;
+    public int age;
 
-    public Student(String name, String password) {
+    public Student(String name, String password,int age) {
         this.name = name;
         this.password = password;
+        this.age=age;
     }
 }
--- testgit/src/com/test/Test1.java
+++ testgit/src/com/test/Test1.java
@@ -4,8 +4,10 @@ import com.pojo.Student;
 
 public class Test1 {
     public static void main(String[] args) {
-        Student student=new Student("chen","111");
+        Student student=new Student("chen","111",12);
         System.out.println(student.name);
+
+        System.out.println(student.age);
 
     }
 }
